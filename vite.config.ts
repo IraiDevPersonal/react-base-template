bbf0206007192f6f5f8f2dd4e7c9f2e0199c0a31
IraiDevPersonal <ignacio.arr01@gmail.com>
2023-04-22 23:31:50 -0400

    fix import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    require("tailwind-scrollbar")({ nocompatible: true })
-  ],
+  plugins: [react()],
   resolve: {
     alias: [
       {
